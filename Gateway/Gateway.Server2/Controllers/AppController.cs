6a55cfe981c7424188166f96f98d591fca06c7be
ugurkilic91 <ugurkilic91@gmail.com>
2020-11-08 16:34:57 +0300

    Gateway  ilk commit

---
+++ Gateway/Gateway.Server2/Controllers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Server1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController : ControllerBase
+    {
+        [HttpGet("AppGet")]
+        public string AppGet()
+        {
+            return $"Server 1 port : 20000";
+        }
+    }
+}
