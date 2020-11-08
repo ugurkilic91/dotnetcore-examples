bc7a5f9223a8bbad90a3342f896cf04b3576b8d3
ugurkilic91 <ugurkilic91@gmail.com>
2020-11-08 19:44:04 +0300

    AppSettings.Json veri okuma Controllerdan erişim.

---
+++ AppSettings-Read/AppSettings-Read/AppSettingsClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSettings_Read
+{
+    public class AppSettingsClass
+    {
+        public string V1 { get; set; }
+        public string V2 { get; set; }
+        public List<string> V3list { get; set; }
+    }
+}
---
+++ AppSettings-Read/AppSettings-Read/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AppSettings_Read.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly AppSettingsClass _optionsAppSettingsClass;
+
+        public WeatherForecastController(IOptions<AppSettingsClass> optionsAppSettingsClass)
+        {
+            _optionsAppSettingsClass = optionsAppSettingsClass.Value;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_optionsAppSettingsClass);
+        }
+    }
+}
