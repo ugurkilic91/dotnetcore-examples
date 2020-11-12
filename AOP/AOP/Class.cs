642a87e8e37cb56b9f15c5bb7d7916de4827d18a
ugurkilic91 <ugurkilic91@gmail.com>
2020-11-12 23:53:58 +0300

    AOP Denemeler

---
+++ AOP/AOP/Class.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+
+public class MySampleActionFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        context.Result = new ContentResult()
+        {
+            Content = "Resource unavailable - header not set."
+        };
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(
+                                                context.ModelState);
+        }
+        context.Result = new BadRequestObjectResult(
+                                             context.ModelState);
+        if (context.HttpContext.TraceIdentifier==null)
+        {
+            context.HttpContext.Response.Redirect("http://www.google.com");
+        }
+        //context.Result = new BadRequestObjectResult (context.HttpContext.);
+        //context.Result = new 
+        // Do something before the action executes.
+
+        //MyDebug.Write(MethodBase.GetCurrentMethod(), context.HttpContext.Request.Path);
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        // Do something after the action executes.
+        //MyDebug.Write(MethodBase.GetCurrentMethod(), context.HttpContext.Request.Path);
+    }
+}
