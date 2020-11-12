b0b3a94abc91ddb80dded17ac0846a53255f6195
ugurkilic91 <ugurkilic91@gmail.com>
2020-11-12 21:39:44 +0300

    Dapper Kullanma Örneği

---
+++ DapperApp/DapperApp/Controllers/KullaniciController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using DapperApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace DapperApp.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class KullaniciController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private string conStr;
+        public KullaniciController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            conStr = _configuration.GetConnectionString("DapperAppConStr");
+        }
+        [HttpGet()]
+        public ActionResult<IEnumerable<Kullanici>> Get()
+        {
+            IEnumerable<Kullanici> firms = new List<Kullanici>();
+            using (var conn = new SQLiteConnection(conStr))
+            {
+                conn.Open();
+                firms = conn.Query<Kullanici>("SELECT * FROM KULLANICI");
+             
+            }
+            return new ActionResult<IEnumerable<Kullanici>>(firms);
+        }
+    }
+}
---
+++ DapperApp/DapperApp/Entities/Kullanici.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperApp.Entities
+{
+    [Table("KULLANICI")]
+    public class Kullanici
+    {
+        [Column("ID")]
+        public int ID { get; set; }
+        [Column("ADI")]
+        public string Adi { get; set; }
+        [Column("SEHIR")]
+        public string Sehir { get; set; }
+    }
+}
