4bae05b4bc367e8dc721799f15b110350ae53f85
ugurkilic91 <ugurkilic91@gmail.com>
2020-12-06 00:21:48 +0300

    Default .net core

---
+++ MyProject/MyFluentValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFluentValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyProject/MyProject.Api/Validator/FluentValidation/WeatherForecastValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Api.Validator.FluentValidation
+{
+    public class WeatherForecastValidator : AbstractValidator<WeatherForecast>
+    {
+        public WeatherForecastValidator()
+        {
+            RuleFor(p => p.TemperatureC).NotEmpty().WithMessage("Buralar hep boş");
+            RuleFor(p => p.Summary).Length(5);
+        }
+    }
+}
